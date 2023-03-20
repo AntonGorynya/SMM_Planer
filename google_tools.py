23304e3538b30d2cff63df19db2ec46d0b31f3a3
SD <antongorynya1@gmail.com>
2023-03-20 20:02:11 +0300

    better formating

--- google_tools.py
+++ google_tools.py
@@ -62,12 +62,8 @@ def delete_event(service, callendar_id, event_id):
 
 def get_credentials(scopes):
     creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', scopes)
-    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -75,7 +71,6 @@ def get_credentials(scopes):
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
     return creds
--- telegram_tools.py
+++ telegram_tools.py
@@ -3,7 +3,6 @@ from pyrogram import Client, types, raw
 from _datetime import datetime
 
 
-
 async def post_photo(app, image, description, date, channel):
     async with app:
         await app.send_photo(channel, photo=image, caption=description, schedule_date=date)
@@ -16,10 +15,8 @@ if __name__ == '__main__':
     tg_app_api_hash = env('TG_APP_API_HASH')
     tg_cahannel = env('TG_CHAT_ID')
     tg_admin = env('TG_ADMIN')
-
     app = Client(name=tg_admin, api_id=tg_api_id, api_hash=tg_app_api_hash)
     date = datetime(year=2023, month=3, day=17, hour=22, minute=6)
     print(date)
     image = "https://cs14.pikabu.ru/post_img/2023/03/16/11/1678996500118751201.webp"
-
     app.run(post_photo(app, image, 'description', date, tg_cahannel))
